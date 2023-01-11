78ba60a96cc47e4888af200ac4e762f1bd45688e
Dima <krutkov.d.v@gmail.com>
2023-01-11 17:41:00 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+"use strict";
+
+// let number = 4.54;
+
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const person = 'Alex glkfdjglkdfgjl 23423 f jflsjf  234 ldkfj ';
+
+// const bool = true;
+
+// // console.log(something);
+
+// let und;
+// console.log(und);
+// 
+// const obj = {
+//     name:"jonh",
+//     age: 25,
+//     isMarried: false
+// };
+
+// //console.log(obj.name);
+// console.log(obj ["name"]);
+
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp', 10, {}, []];
+// console.log(arr[1]);
+
+
+// // урок 007 
+// //alert('Hello');
+
+// //const result = confirm("Are you here?");
+// //console.log(result);
+
+// //const answer = +prompt("Вам есть 18?", "18");
+// //console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('Как Ваше имя?', '');
+// answers[1] = prompt('Фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// урок 8. Интерполяция
+
+//  const category = 'toys';
+
+//  console.log(`https://someurl.com/${category}/5445`);
+
+//  const user = 'Dima';
+//  alert(`Привет, ${user}`);
+
+// Урок 9. Операторы JS
+
+console.log('arr' + " - object");
